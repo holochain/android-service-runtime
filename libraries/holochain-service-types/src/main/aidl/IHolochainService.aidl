53672126cf304d7747118f51439298c2780b8dd6
Matt Gabrenya <matt@buildyourweb.app>
2025-03-06 21:46:36 -0700

    refactor: tauri plugins both use HolochainServiceClient, clean up aidl types and type casting

--- libraries/holochain-service-types/src/main/aidl/IHolochainService.aidl
+++ libraries/holochain-service-types/src/main/aidl/IHolochainService.aidl
@@ -1,20 +1,28 @@
 package com.holochain_apps.holochain_service_types;
 
-parcelable InstallAppRequestAidl;
+parcelable InstallAppPayloadFfiParcel;
 parcelable ListInstalledAppsResponse;
-parcelable AppInfoFfiAidl;
-parcelable AppWebsocketAuthFfiAidl;
-parcelable SignZomeCallRequestAidl;
-parcelable ZomeCallSignedFfiAidl;
+parcelable AppInfoFfiParcel;
+parcelable AppWebsocketFfiParcel;
+parcelable ZomeCallUnsignedFfiParcel;
+parcelable ZomeCallFfiParcel;
 
 interface IHolochainService {
-    void shutdown();
-    void installApp(in InstallAppRequestAidl request);
-    void uninstallApp(String appId);
-    void enableApp(String appId);
-    void disableApp(String appId);
-    List<AppInfoFfiAidl> listInstalledApps();
-    boolean isAppInstalled(String appId);
-    AppWebsocketAuthFfiAidl ensureAppWebsocket(String appId);
-    ZomeCallSignedFfiAidl signZomeCall(in SignZomeCallRequestAidl request);
+    void stop();
+    AppInfoFfiParcel installApp(in InstallAppPayloadFfiParcel request);
+    void uninstallApp(String installedAppId);
+    AppInfoFfiParcel enableApp(String installedAppId);
+    void disableApp(String installedAppId);
+    List<AppInfoFfiParcel> listApps();
+    boolean isAppInstalled(String installedAppId);
+    AppWebsocketFfiParcel ensureAppWebsocket(String installedAppId);
+    ZomeCallFfiParcel signZomeCall(in ZomeCallUnsignedFfiParcel request);
+}
+
+interface IHolochainCallbackService {
+    void installAppCallback(AppInfoFfiParcel response);
+    void enableAppCallback(AppInfoFfiParcel response);
+    void listAppsCallback(List<AppInfoFfiParcel> response);
+    void ensureAppWebsocket(AppWebsocketFfiParcel response);
+    void signZomeCall(ZomeCallFfiParcel response);
 }
---
+++ libraries/holochain-service-types/src/main/java/com/holochain_apps/holochain_service_types/HolochainServiceClient.kt
@@ -0,0 +1,85 @@
+package com.holochain_apps.holochain_service_types
+
+import android.app.Activity
+import android.content.ServiceConnection
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class HolochainServiceClient(
+    private val activity: Activity,
+    val serviceName: String = "com.plugin.holochain_service.HolochainService",
+    val servicePackageName: String = "com.holochain_apps.android_service_runtime"
+) {
+    private var mService: IHolochainService? = null
+    private val logTag = "HolochainServiceClient"
+
+    // IPC Connection to HolochainService using AIDL
+    private val mConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            mService = IHolochainService.Stub.asInterface(service)
+            Log.d(logTag, "IHolochainService connected")
+        }
+
+        override fun onServiceDisconnected(className: ComponentName) {
+            mService = null
+            Log.d(logTag, "IHolochainService disconnected")
+        }
+    }
+
+    /// Start the service
+    fun connect() {
+        val intent = Intent(this.serviceName)
+        intent.setPackage(this.servicePackageName)
+
+        this.activity.startForegroundService(intent)
+        this.activity.bindService(intent, this.mConnection, Context.BIND_IMPORTANT)
+    }
+    
+    /// Stop the service
+    fun stop() {
+        this.mService!!.stop()
+    }
+
+    /// Install an app
+    fun installApp(payload: InstallAppPayloadFfiParcel): AppInfoFfiParcel {
+        // Call installApp on service
+        return this.mService!!.installApp(payload)
+    }
+
+    /// Is an app with the given app_id installed
+    fun isAppInstalled(installedAppId: String): Boolean {
+        return this.mService!!.isAppInstalled(installedAppId)
+    }
+
+    /// Uninstall an installed app
+    fun uninstallApp(installedAppId: String) {
+        this.mService!!.uninstallApp(installedAppId)
+    }
+
+    /// Enable an installed app
+    fun enableApp(installedAppId: String) {
+        this.mService!!.enableApp(installedAppId)
+    }
+
+    /// Disable an installed app
+    fun disableApp(installedAppId: String) {
+        this.mService!!.disableApp(installedAppId)
+    }
+
+    /// List installed happs in conductor
+    fun listApps(): MutableList<AppInfoFfiParcel> {
+        return this.mService!!.listApps()
+    }
+
+    /// Get or create an app websocket with authentication token
+    fun ensureAppWebsocket(installedAppId: String): AppWebsocketFfiParcel {
+        return this.mService!!.ensureAppWebsocket(installedAppId)
+    }
+
+    fun signZomeCall(args: ZomeCallUnsignedFfiParcel): ZomeCallFfiParcel {
+        return this.mService!!.signZomeCall(args);
+    }
+}
