68c9dadaaa73f839dc5bbd7ae5c7501a9890d3f0
Matt Gabrenya <matt@buildyourweb.app>
2025-04-04 13:06:13 -0700

    chore: fmt

--- libraries/holochain-service-client/src/androidTest/kotlin/JsonTest.kt
+++ libraries/holochain-service-client/src/androidTest/kotlin/JsonTest.kt
@@ -1,15 +1,10 @@
 package com.plugin.holochain_service
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.Assert.*
 import kotlin.Byte
 import kotlin.Double
 import kotlin.Float
 import kotlin.random.Random
-import org.json.JSONArray
-import org.json.JSONObject
 import org.holochain.androidserviceruntime.holochain_service_client.AppInfoStatusFfi
 import org.holochain.androidserviceruntime.holochain_service_client.CellIdFfi
 import org.holochain.androidserviceruntime.holochain_service_client.CellInfoFfi
@@ -22,105 +17,116 @@ import org.holochain.androidserviceruntime.holochain_service_client.ProvisionedC
 import org.holochain.androidserviceruntime.holochain_service_client.RoleSettingsFfi
 import org.holochain.androidserviceruntime.holochain_service_client.toJSONArray
 import org.holochain.androidserviceruntime.holochain_service_client.toJSONObject
+import org.json.JSONArray
+import org.json.JSONObject
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class JsonTest {
 
-    enum class Direction {
-        NORTH, SOUTH, WEST, EAST
-    }
+  enum class Direction {
+    NORTH,
+    SOUTH,
+    WEST,
+    EAST
+  }
 
-    @Test
-    fun testAppInfoStatusFfiDisabled() {
-        data class MyObj(
-            var status: AppInfoStatusFfi
-        )
+  @Test
+  fun testAppInfoStatusFfiDisabled() {
+    data class MyObj(var status: AppInfoStatusFfi)
 
-        val value = MyObj(status = AppInfoStatusFfi.Disabled(DisabledAppReasonFfi.NeverStarted))
-        val res = JSONObject(value.toJSONObject().toString())
+    val value = MyObj(status = AppInfoStatusFfi.Disabled(DisabledAppReasonFfi.NeverStarted))
+    val res = JSONObject(value.toJSONObject().toString())
 
-        assertEquals(res.getJSONObject("status").getString("type"), "Disabled")
-        assertEquals(res.getJSONObject("status").getJSONObject("reason").getString("type"), "NeverStarted")
-    }
+    assertEquals(res.getJSONObject("status").getString("type"), "Disabled")
+    assertEquals(
+        res.getJSONObject("status").getJSONObject("reason").getString("type"), "NeverStarted")
+  }
 
-    @Test
-    fun testAppInfoStatusFfiPaused() {
-        data class MyObj(
-            var status: AppInfoStatusFfi
-        )
+  @Test
+  fun testAppInfoStatusFfiPaused() {
+    data class MyObj(var status: AppInfoStatusFfi)
 
-        val value = MyObj(status = AppInfoStatusFfi.Paused(PausedAppReasonFfi.Error("my error")))
-        val res = JSONObject(value.toJSONObject().toString())
+    val value = MyObj(status = AppInfoStatusFfi.Paused(PausedAppReasonFfi.Error("my error")))
+    val res = JSONObject(value.toJSONObject().toString())
 
-        assertEquals(res.getJSONObject("status").getString("type"), "Paused")
-        assertEquals(res.getJSONObject("status").getJSONObject("reason").getString("type"), "Error")
-        assertEquals(res.getJSONObject("status").getJSONObject("reason").getString("v1"), "my error")
-    }
+    assertEquals(res.getJSONObject("status").getString("type"), "Paused")
+    assertEquals(res.getJSONObject("status").getJSONObject("reason").getString("type"), "Error")
+    assertEquals(res.getJSONObject("status").getJSONObject("reason").getString("v1"), "my error")
+  }
 
-    @Test
-    fun testCellInfoFfi() {
-        data class MyObj(
-            var info: CellInfoFfi
-        )
+  @Test
+  fun testCellInfoFfi() {
+    data class MyObj(var info: CellInfoFfi)
 
-        val value = MyObj(info = CellInfoFfi.Provisioned(
-            ProvisionedCellFfi(
-                cellId = CellIdFfi(
-                    dnaHash = ByteArray(32) { Random.nextInt(256).toByte() },
-                    agentPubKey = ByteArray(32) { Random.nextInt(256).toByte() },
-                ),
-                dnaModifiers = DnaModifiersFfi(
-                    networkSeed = "1234",
-                    properties = ByteArray(300) { Random.nextInt(256).toByte() },
-                    originTime = 1000L,
-                    quantumTime = DurationFfi(100UL, 100U)),
-                name = "cell-1")))
-        val res = JSONObject(value.toJSONObject().toString())
+    val value =
+        MyObj(
+            info =
+                CellInfoFfi.Provisioned(
+                    ProvisionedCellFfi(
+                        cellId =
+                            CellIdFfi(
+                                dnaHash = ByteArray(32) { Random.nextInt(256).toByte() },
+                                agentPubKey = ByteArray(32) { Random.nextInt(256).toByte() },
+                            ),
+                        dnaModifiers =
+                            DnaModifiersFfi(
+                                networkSeed = "1234",
+                                properties = ByteArray(300) { Random.nextInt(256).toByte() },
+                                originTime = 1000L,
+                                quantumTime = DurationFfi(100UL, 100U)),
+                        name = "cell-1")))
+    val res = JSONObject(value.toJSONObject().toString())
 
-        assertEquals(res.getJSONObject("info").getString("type"), "Provisioned")
-        assertEquals(res.getJSONObject("info").getJSONObject("v1").getString("name"), "cell-1")
-    }
+    assertEquals(res.getJSONObject("info").getString("type"), "Provisioned")
+    assertEquals(res.getJSONObject("info").getJSONObject("v1").getString("name"), "cell-1")
+  }
 
-    @Test
-    fun testRoleSettingsFfi() {
-        data class MyObj(
-            var rolesettings: RoleSettingsFfi
-        )
+  @Test
+  fun testRoleSettingsFfi() {
+    data class MyObj(var rolesettings: RoleSettingsFfi)
 
-        val value = MyObj(rolesettings = RoleSettingsFfi.Provisioned(
-            membraneProof = ByteArray(60) { Random.nextInt(256).toByte() },
-            modifiers =
-            DnaModifiersOptFfi(
-                networkSeed = "1234",
-                properties =
-                ByteArray(300) { Random.nextInt(256).toByte() },
-                originTime = 1000L,
-                quantumTime = DurationFfi(100UL, 100U)),
-        ))
+    val value =
+        MyObj(
+            rolesettings =
+                RoleSettingsFfi.Provisioned(
+                    membraneProof = ByteArray(60) { Random.nextInt(256).toByte() },
+                    modifiers =
+                        DnaModifiersOptFfi(
+                            networkSeed = "1234",
+                            properties = ByteArray(300) { Random.nextInt(256).toByte() },
+                            originTime = 1000L,
+                            quantumTime = DurationFfi(100UL, 100U)),
+                ))
 
-        val res = JSONObject(value.toJSONObject().toString())
-        assertEquals(res.getJSONObject("rolesettings").getString("type"), "Provisioned")
-        assertEquals(res.getJSONObject("rolesettings").getJSONObject("modifiers").getString("networkSeed"), "1234")
-    }
+    val res = JSONObject(value.toJSONObject().toString())
+    assertEquals(res.getJSONObject("rolesettings").getString("type"), "Provisioned")
+    assertEquals(
+        res.getJSONObject("rolesettings").getJSONObject("modifiers").getString("networkSeed"),
+        "1234")
+  }
 
-    @Test
-    fun testPrimitives() {
-        data class MyObj(
-            var string: String,
-            var int: Int,
-            var uint: UInt,
-            var long: Long,
-            var ulong: ULong,
-            var double: Double,
-            var float: Float,
-            var boolean: Boolean,
-            var byte: Byte,
-            var ubyte: UByte,
-            var myEnum: Direction,
-            var empty: Int?,
-            var byteArray: ByteArray
-        )
-        val value = MyObj(
+  @Test
+  fun testPrimitives() {
+    data class MyObj(
+        var string: String,
+        var int: Int,
+        var uint: UInt,
+        var long: Long,
+        var ulong: ULong,
+        var double: Double,
+        var float: Float,
+        var boolean: Boolean,
+        var byte: Byte,
+        var ubyte: UByte,
+        var myEnum: Direction,
+        var empty: Int?,
+        var byteArray: ByteArray
+    )
+    val value =
+        MyObj(
             string = "my string",
             int = 100,
             uint = 100U,
@@ -135,56 +141,50 @@ class JsonTest {
             empty = null,
             byteArray = byteArrayOf(0x0, 0x1, 0x2),
         )
-        val res = JSONObject(value.toJSONObject().toString())
+    val res = JSONObject(value.toJSONObject().toString())
 
-        assertEquals(res.getString("string"), "my string")
-        assertEquals(res.getInt("int"), 100)
-        assertEquals(res.getInt("uint"), 100)
-        assertEquals(res.getInt("long"), 100)
-        assertEquals(res.getInt("ulong"), 100)
-        assertEquals(res.getDouble("double"), 100.0, 0.0)
-        assertEquals(res.getDouble("float"), 100.0, 0.0)
-        assertEquals(res.getBoolean("boolean"), true)
-        assertEquals(res.getInt("byte"), 0)
-        assertEquals(res.getString("myEnum"), "NORTH")
-        assertNull(res.optString("empty", null))
-        assertEquals(res.getJSONArray("byteArray")[0], JSONArray(arrayOf(0, 1, 2, 3))[0])
-        assertEquals(res.getJSONArray("byteArray")[1], JSONArray(arrayOf(0, 1, 2, 3))[1])
-        assertEquals(res.getJSONArray("byteArray")[2], JSONArray(arrayOf(0, 1, 2, 3))[2])
-    }
+    assertEquals(res.getString("string"), "my string")
+    assertEquals(res.getInt("int"), 100)
+    assertEquals(res.getInt("uint"), 100)
+    assertEquals(res.getInt("long"), 100)
+    assertEquals(res.getInt("ulong"), 100)
+    assertEquals(res.getDouble("double"), 100.0, 0.0)
+    assertEquals(res.getDouble("float"), 100.0, 0.0)
+    assertEquals(res.getBoolean("boolean"), true)
+    assertEquals(res.getInt("byte"), 0)
+    assertEquals(res.getString("myEnum"), "NORTH")
+    assertNull(res.optString("empty", null))
+    assertEquals(res.getJSONArray("byteArray")[0], JSONArray(arrayOf(0, 1, 2, 3))[0])
+    assertEquals(res.getJSONArray("byteArray")[1], JSONArray(arrayOf(0, 1, 2, 3))[1])
+    assertEquals(res.getJSONArray("byteArray")[2], JSONArray(arrayOf(0, 1, 2, 3))[2])
+  }
 
-    @Test
-    fun testMap() {
-        data class MyObj(
-            var map: Map<String, String>
-        )
-        val value = MyObj(map = mapOf("my key" to "my value"))
-        val res = JSONObject(value.toJSONObject().toString())
+  @Test
+  fun testMap() {
+    data class MyObj(var map: Map<String, String>)
+    val value = MyObj(map = mapOf("my key" to "my value"))
+    val res = JSONObject(value.toJSONObject().toString())
 
-        assertEquals(res.getJSONObject("map").getString("my key"), "my value")
-    }
+    assertEquals(res.getJSONObject("map").getString("my key"), "my value")
+  }
 
-    @Test
-    fun testCollection() {
-        data class MyObj(
-            var arr: List<String>
-        )
-        val value = MyObj(arr = listOf("my string 1", "my string 2"))
-        val res = JSONObject(value.toJSONObject().toString())
+  @Test
+  fun testCollection() {
+    data class MyObj(var arr: List<String>)
+    val value = MyObj(arr = listOf("my string 1", "my string 2"))
+    val res = JSONObject(value.toJSONObject().toString())
 
-        assertEquals(res.getJSONArray("arr").getString(0), "my string 1")
-        assertEquals(res.getJSONArray("arr").getString(1), "my string 2")
-    }
+    assertEquals(res.getJSONArray("arr").getString(0), "my string 1")
+    assertEquals(res.getJSONArray("arr").getString(1), "my string 2")
+  }
 
-    @Test
-    fun testArray() {
-        data class MyObj(
-            var str: String
-        )
-        val value = listOf(MyObj(str = "my string 1"), MyObj(str = "my string 2"))
-        val res = JSONArray(value.toJSONArray().toString())
+  @Test
+  fun testArray() {
+    data class MyObj(var str: String)
+    val value = listOf(MyObj(str = "my string 1"), MyObj(str = "my string 2"))
+    val res = JSONArray(value.toJSONArray().toString())
 
-        assertEquals(res.getJSONObject(0).getString("str"), "my string 1")
-        assertEquals(res.getJSONObject(1).getString("str"), "my string 2")
-    }
+    assertEquals(res.getJSONObject(0).getString("str"), "my string 1")
+    assertEquals(res.getJSONObject(1).getString("str"), "my string 2")
+  }
 }
--- libraries/holochain-service-client/src/main/java/org/holochain/androidserviceruntime/holochain_service_client/Json.kt
+++ libraries/holochain-service-client/src/main/java/org/holochain/androidserviceruntime/holochain_service_client/Json.kt
@@ -7,119 +7,133 @@
 /// Note that sealed classes must be matched explicitly
 package org.holochain.androidserviceruntime.holochain_service_client
 
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.KProperty1
 import android.util.Log
-import org.holochain.androidserviceruntime.holochain_service_client.AppInfoStatusFfi
-import org.holochain.androidserviceruntime.holochain_service_client.CellInfoFfi
-import org.holochain.androidserviceruntime.holochain_service_client.DisabledAppReasonFfi
-import org.holochain.androidserviceruntime.holochain_service_client.PausedAppReasonFfi
-import org.holochain.androidserviceruntime.holochain_service_client.RoleSettingsFfi
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
 import org.json.JSONArray
 import org.json.JSONObject
 
 object Json {
 
-    /// Convert Any object to a JSONObject
-    @OptIn(ExperimentalUnsignedTypes::class)
-    fun toJSONPrimitive(value: Any?): Any? {
-        return when (value) {
-            null -> null
-            is String, is Int, is Long, is Double, is Boolean, is ULong, is UInt -> {
-                value
-            }
-            is Float -> {
-                value.toDouble()
-            }
-            is Byte -> {
-                value.toInt()
-            }
-            is UByte -> {
-                value.toInt()
-            }
-            is Enum<*> -> {
-                value.name
-            }
-            is Map<*,*> -> {
-                var map = HashMap<String, Any?>()
-                value.forEach { entry ->
-                    try {
-                        map.put(entry.key as String, toJSONPrimitive(entry.value))
-                    } catch (e: Exception) {
-                        Log.e("toJSONObject", "Error converting Map entry ${entry.key} with value ${entry.value} to JSONObject", e)
-                    }
-                }
-                JSONObject(map) as Any
-            }
-            is ByteArray -> {
-                val byteCollection: MutableCollection<UByte> = value.toUByteArray().toMutableList()
-                val jsValue = try {
-                    (byteCollection as? Collection<UByte>)?.toJSONArray()
-                } catch (e: Exception) {
-                    Log.e("toJSONObject", "Error converting $value to toJSONArray", e)
-                    null
-                }
-                jsValue
-            }
-            is Collection<*> -> {
-                val jsValue = try {
-                    (value.map { it as Any }).toJSONArray()
-                } catch (e: Exception) {
-                    Log.e("toJSONObject", "Error converting $value to toJSONArray", e)
-                    null
-                }
-                jsValue
-            }
-            // Is this a known sealed class (i.e. converted from a Rust enum)?
-            is AppInfoStatusFfi, is CellInfoFfi, is DisabledAppReasonFfi, is PausedAppReasonFfi, is RoleSettingsFfi -> {
-                val jsValue = try {
-                    value.toJSONObject()
-                } catch (e: Exception) {
-                    Log.e("toJSONObject", "Error converting $value to JSONObject", e)
-                    null
-                }
-                jsValue?.put("type", value::class.simpleName)
-                jsValue
-            }
-            else -> {
-                val jsValue = try {
-                    value.toJSONObject()
-                } catch (e: Exception) {
-                    Log.e("toJSONObject", "Error converting $value to JSONObject", e)
-                    null
-                }
-                jsValue
-            }
+  /// Convert Any object to a JSONObject
+  @OptIn(ExperimentalUnsignedTypes::class)
+  fun toJSONPrimitive(value: Any?): Any? {
+    return when (value) {
+      null -> null
+      is String,
+      is Int,
+      is Long,
+      is Double,
+      is Boolean,
+      is ULong,
+      is UInt -> {
+        value
+      }
+      is Float -> {
+        value.toDouble()
+      }
+      is Byte -> {
+        value.toInt()
+      }
+      is UByte -> {
+        value.toInt()
+      }
+      is Enum<*> -> {
+        value.name
+      }
+      is Map<*, *> -> {
+        var map = HashMap<String, Any?>()
+        value.forEach { entry ->
+          try {
+            map.put(entry.key as String, toJSONPrimitive(entry.value))
+          } catch (e: Exception) {
+            Log.e(
+                "toJSONObject",
+                "Error converting Map entry ${entry.key} with value ${entry.value} to JSONObject",
+                e)
+          }
         }
+        JSONObject(map) as Any
+      }
+      is ByteArray -> {
+        val byteCollection: MutableCollection<UByte> = value.toUByteArray().toMutableList()
+        val jsValue =
+            try {
+              (byteCollection as? Collection<UByte>)?.toJSONArray()
+            } catch (e: Exception) {
+              Log.e("toJSONObject", "Error converting $value to toJSONArray", e)
+              null
+            }
+        jsValue
+      }
+      is Collection<*> -> {
+        val jsValue =
+            try {
+              (value.map { it as Any }).toJSONArray()
+            } catch (e: Exception) {
+              Log.e("toJSONObject", "Error converting $value to toJSONArray", e)
+              null
+            }
+        jsValue
+      }
+      // Is this a known sealed class (i.e. converted from a Rust enum)?
+      is AppInfoStatusFfi,
+      is CellInfoFfi,
+      is DisabledAppReasonFfi,
+      is PausedAppReasonFfi,
+      is RoleSettingsFfi -> {
+        val jsValue =
+            try {
+              value.toJSONObject()
+            } catch (e: Exception) {
+              Log.e("toJSONObject", "Error converting $value to JSONObject", e)
+              null
+            }
+        jsValue?.put("type", value::class.simpleName)
+        jsValue
+      }
+      else -> {
+        val jsValue =
+            try {
+              value.toJSONObject()
+            } catch (e: Exception) {
+              Log.e("toJSONObject", "Error converting $value to JSONObject", e)
+              null
+            }
+        jsValue
+      }
     }
+  }
 
-    fun Any?.toJsonPrimitive(): Any? = toJSONPrimitive(this)
+  fun Any?.toJsonPrimitive(): Any? = toJSONPrimitive(this)
 
-    /// Convert Any object to a JSONObject
-    @OptIn(ExperimentalUnsignedTypes::class)
-    inline fun <reified T : Any> toJSONObject(data: T): JSONObject {
-        val obj = JSONObject()
-        val properties = data::class.memberProperties
-        for (property in properties) {
-            val prop = property as? KProperty1<T, *>
-            val value = prop?.get(data)
-            obj.put(property.name, value.toJsonPrimitive())
-        }
-        return obj
+  /// Convert Any object to a JSONObject
+  @OptIn(ExperimentalUnsignedTypes::class)
+  inline fun <reified T : Any> toJSONObject(data: T): JSONObject {
+    val obj = JSONObject()
+    val properties = data::class.memberProperties
+    for (property in properties) {
+      val prop = property as? KProperty1<T, *>
+      val value = prop?.get(data)
+      obj.put(property.name, value.toJsonPrimitive())
     }
+    return obj
+  }
 
-    /// Convert Collection<Any> to a JSONArray
-    inline fun <reified T : Collection<Any>> toJSONArray(data: T): JSONArray {
-        val arr = JSONArray()
-        for (element in data) {
-            arr.put(element.toJsonPrimitive())
-        }
-        return arr
+  /// Convert Collection<Any> to a JSONArray
+  inline fun <reified T : Collection<Any>> toJSONArray(data: T): JSONArray {
+    val arr = JSONArray()
+    for (element in data) {
+      arr.put(element.toJsonPrimitive())
     }
+    return arr
+  }
 }
 
 fun Any.toJSONObject(): JSONObject = Json.toJSONObject(this)
+
 fun Collection<Any>.toJSONArray(): JSONArray = Json.toJSONArray(this)
 
 fun Any.toJSONObjectString(): String = Json.toJSONObject(this).toString()
+
 fun Collection<Any>.toJSONArrayString(): String = Json.toJSONArray(this).toString()
