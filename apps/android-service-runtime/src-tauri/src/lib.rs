0518e14864ef3f5b562e32ddf89ff41f0fea7055
Matt Gabrenya <matt@buildyourweb.app>
2025-04-04 21:18:59 -0700

    fix: ignore error that logger is initialized twice

--- apps/android-service-runtime/src-tauri/src/lib.rs
+++ apps/android-service-runtime/src-tauri/src/lib.rs
@@ -5,22 +5,15 @@ use std::sync::Once;
 use simple_logger::SimpleLogger;
 use log::LevelFilter;
 
-// Prevent the logger from being initialized twice
-// when the app is closed and re-launched
 static LOGGER: LazyLock<SimpleLogger> = LazyLock::new(|| {
     SimpleLogger::new()
 });
-static INIT_LOGGER: Once = Once::new();
-fn init_logger() {
-    INIT_LOGGER.call_once(|| {
-        log::set_logger(&*LOGGER).unwrap();
-        log::set_max_level(LevelFilter::Warn);
-    });
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    init_logger();
+    // Ignore error that logger is already initialized
+    let _ = log::set_logger(&*LOGGER);
+    log::set_max_level(LevelFilter::Warn);
 
     tauri::Builder::default()
         .plugin(tauri_plugin_holochain_service::init())
