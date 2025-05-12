dca1ecba7a7859494753bf659c712baf99286460
Matt Gabrenya <matt@buildyourweb.app>
2025-05-12 16:51:55 -0700

    revert: unrelated changes

--- crates/tauri-plugin-client/build.rs
+++ crates/tauri-plugin-client/build.rs
@@ -3,5 +3,6 @@ const COMMANDS: &[&str] = &["connect_setup_app", "enable_app", "sign_zome_call"]
 fn main() {
     tauri_plugin::Builder::new(COMMANDS)
         .android_path("android")
+        .ios_path("ios")
         .build();
 }
