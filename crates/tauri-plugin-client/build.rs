b2bf8f8ce180b282bedce0e452c27db56cfc938c
Matt Gabrenya <matt@buildyourweb.app>
2025-05-12 16:37:25 -0700

    chore: rm unused ios path

--- crates/tauri-plugin-client/build.rs
+++ crates/tauri-plugin-client/build.rs
@@ -3,6 +3,5 @@ const COMMANDS: &[&str] = &["connect_setup_app", "enable_app", "sign_zome_call"]
 fn main() {
     tauri_plugin::Builder::new(COMMANDS)
         .android_path("android")
-        .ios_path("ios")
         .build();
 }
