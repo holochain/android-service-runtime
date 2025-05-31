c2b1e909b2f455eb1cde0af3b14646ff6b810664
Matt Gabrenya <matt@buildyourweb.app>
2025-05-30 17:46:37 -0700

    fix: copy librarys path and ignore if already in destinatoin

--- libraries/service/scripts/build.sh
+++ libraries/service/scripts/build.sh
@@ -4,9 +4,9 @@
 #
 # This is a dependency of the holochain-conductor-runtime crate, because of holochain's dependency on a c++ library libdatachannel.
 
-cp $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/libc++_shared.so src/main/jniLibs/arm64-v8a/
-cp $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/x86_64-linux-android/libc++_shared.so src/main/jniLibs/x86_64/
-cp $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/i386-linux-android/libc++_shared.so src/main/jniLibs/x86/
+cp --no-clobber $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/libc++_shared.so src/main/jniLibs/arm64-v8a/
+cp --no-clobber $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/x86_64-linux-android/libc++_shared.so src/main/jniLibs/x86_64/
+cp --no-clobber $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/i686-linux-android/libc++_shared.so src/main/jniLibs/x86/
 
 # Run gradle build
 ./gradlew build
